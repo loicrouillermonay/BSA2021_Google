cef7fa87a2d4bc8b6325beda1abe1a7e66e4f331
Loïc Rouiller-Monay <65773911+loicrouillermonay@users.noreply.github.com>
2021-04-10 14:44:09 +0200

    api working on localhost

---
+++ api/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, jsonify
+import os
+import torch
+from transformers import CamembertForSequenceClassification, CamembertTokenizer, AdamW, get_linear_schedule_with_warmup
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+def get_model():
+    state_dict = torch.load("lingorank_v1", map_location=torch.device('cpu'))
+    model = CamembertForSequenceClassification.from_pretrained(
+        'camembert-base',
+        state_dict=state_dict,
+        num_labels=6)
+    return model
+
+
+def preprocess(raw_texts, labels=None):
+    TOKENIZER = CamembertTokenizer.from_pretrained(
+        'camembert-base', do_lower_case=True)
+    encoded_batch = TOKENIZER.batch_encode_plus(raw_texts,
+                                                add_special_tokens=True,
+                                                pad_to_max_length=True,
+                                                return_attention_mask=True,
+                                                return_tensors='pt')
+    if labels:
+        labels = torch.tensor(labels)
+        return encoded_batch['input_ids'], encoded_batch['attention_mask'], labels
+    return encoded_batch['input_ids'], encoded_batch['attention_mask']
+
+
+def predict(texts, model):
+    with torch.no_grad():
+        model.eval()
+        input_ids, attention_mask = preprocess(texts)
+        retour = model(input_ids, attention_mask=attention_mask)
+        return torch.argmax(retour[0], dim=1)
+
+
+model = get_model()
+difficulties = {0: 'A1', 1: 'A2', 2: 'B1', 3: 'B2', 4: 'C1', 5: 'C2'}
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "BSA2021 - Team Google's API is working!"
+
+
+@app.route('/api/predict', methods=['GET'])
+def make_prediction():
+    text = request.args.get("text", "")
+    prediction = predict([text], model)
+    return jsonify({'text': text, 'difficulty': difficulties[int(prediction)]})
+
+
+@app.route('/api/predict', methods=['GET'])
+def make_prediction_of_list():
+    text = request.args.get("text", "")
+    prediction = predict([text], model)
+    return jsonify({'texts': text, 'difficulty': difficulties[int(prediction)]})
+
+
+if __name__ == '__main__':
+    app.run()
