55062326dc2859703d828b3fc690661cf3ee69a7
Francis R <guyleguain@windowslive.com>
2021-05-07 15:38:34 +0200

    + UI and individual word difficulty

--- frontend/app.py
+++ frontend/app.py
@@ -21,41 +21,65 @@ def main():
     st.markdown("### Lingorank predicts the difficulty of a French written sentence.")
     st.markdown(" Enter your French sentence below")
 
-    difficulties = [0,0,0,0,0,0,1,1,1,0,2,3,4,5,0,1,2,3,4,5]
-
     # Creating a text box for user input
     sentence = st.text_area(
-        "", "Remplacez cette phrase par celle que vous voulez faire analyser. Remplacez cette phrase par celle que vous voulez faire.")
+        "", "Remplacez cette phrase par celle que vous voulez faire analyser.")
 
+    # Create the button
     if st.button("Classify"):
-        with st.spinner(text='In progress...'):
+        # On click, display waiting message while querying
+        with st.spinner(text='Processing overall sentence difficulty...'):
+            # Query request and response with overall difficulty
             query = {'text': sentence}
             response = requests.get(
-                'http://51.103.141.180/api/predict', params=query)
-            #st.success(
-            #    f"Difficulty categorized as: {response.json()['difficulty']}")
+                'http://51.103.167.205/api/predict', params=query)
+                # Display the result of the query
+            st.success(
+                f"Succesfully categorized overall difficulty as: **{response.json()['difficulty']}**")
+        # After the success of the previous operation, perform a second query with individual words difficulty
+        with st.spinner(text='Processing individual words difficulty'):
+            response_diff = requests.get(
+                'http://51.103.167.205/api/predict/words', params=query)
+            # Assign list of individual difficulty to a variable
+            difficulties = response_diff.json()['difficulty']
 
-    st.markdown("---")
+            # Some layout
+            caption = '''<hr><h4>Find here the individual difficulty of words. Background color varies depending on the level.</h4> 
+            <i>NOTA BENE :</i> Level is usually low because words are analyzed without their context 
+            <br> <br>
+            <div style="text-align:center"> Caption : 
+            <span style = 'background-color:#2E541A'>[ A1 ]</span> - 
+            <span style = 'background-color:#6C8A1A'>[ A2 ]</span> - 
+            <span style = 'background-color:#F7C32D'>[ B1 ]</span> - 
+            <span style = 'background-color:#DE700F'>[ B2 ]</span> - 
+            <span style = 'background-color:#B21F01'>[ C1 ]</span> - 
+            <span style = 'background-color:#7B1200'>[ C2 ]</span>
+            </div> <br>
+            '''
+            
+            # Prepare variables to display the sentence with individual difficulties
+            index = 0
+            bloc = sentence.split(' ')
+            output = ""
+            for i in bloc:
+                if(difficulties[index] == 0):
+                    output = output + "<span style = 'background-color:#2E541A'>" + bloc[index] + "</span>" + " "
+                if(difficulties[index] == 1):
+                    output = output + "<span style = 'background-color:#6C8A1A'>" + bloc[index] + "</span>" + " "
+                if(difficulties[index] == 2):
+                    output = output + "<span style = 'background-color:#F7C32D'>" + bloc[index] + "</span>" + " "
+                if(difficulties[index] == 3):
+                    output = output + "<span style = 'background-color:#DE700F'>" + bloc[index] + "</span>" + " "
+                if(difficulties[index] == 4):
+                    output = output + "<span style = 'background-color:#B21F01'>" + bloc[index] + "</span>" + " "
+                if(difficulties[index] == 5):
+                    output = output + "<span style = 'background-color:#7B1200'>" + bloc[index] + "</span>" + " "
 
-    index = 0
-    bloc = sentence.split(' ')
-    output = ""
-    for i in bloc:
-        if(difficulties[index] == 0):
-            output = output + "<span style = 'background-color:#2E541A'>" + bloc[index] + "</span>" + " "
-        if(difficulties[index] == 1):
-            output = output + "<span style = 'background-color:#6C8A1A'>" + bloc[index] + "</span>" + " "
-        if(difficulties[index] == 2):
-            output = output + "<span style = 'background-color:#F7C32D'>" + bloc[index] + "</span>" + " "
-        if(difficulties[index] == 3):
-            output = output + "<span style = 'background-color:#DE700F'>" + bloc[index] + "</span>" + " "
-        if(difficulties[index] == 4):
-            output = output + "<span style = 'background-color:#B21F01'>" + bloc[index] + "</span>" + " "
-        if(difficulties[index] == 5):
-            output = output + "<span style = 'background-color:#7B1200'>" + bloc[index] + "</span>" + " "
-
-        index = index + 1
-    st.markdown(output, unsafe_allow_html=True)
+                index = index + 1
+            # Display caption and sentence with indificual difficulties
+            st.markdown(caption, unsafe_allow_html=True)
+            output = "<i>" + output + "</i>"
+            st.markdown(output, unsafe_allow_html=True)
     
 
     
