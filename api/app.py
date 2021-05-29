f8bd5df2a04925014022f0d0852a462b054d67a6
Loïc Rouiller-Monay <65773911+loicrouillermonay@users.noreply.github.com>
2021-05-29 22:50:22 +0200

    Update app.py

--- api/app.py
+++ api/app.py
@@ -14,6 +14,11 @@ app.config["DEBUG"] = False
 
 
 def get_model():
+    """ Load a trained model.
+
+    Returns:
+        CamembertForSequenceClassification: model class
+    """
     state_dict = torch.load("lingorank.pt", map_location=torch.device('cpu'))
     model = CamembertForSequenceClassification.from_pretrained(
         'camembert-base',
@@ -23,6 +28,15 @@ def get_model():
 
 
 def preprocess(raw_texts, labels=None):
+    """ Takes raw data as argument and returns a pytorch dataloader.
+
+    Args:
+        raw_texts (array-like) : A list of texts in the form of 'str'
+        labels : a labels list from 0 to 5
+
+    Returns:
+        inputs_ids, attention_masks, labels(optionel) : PyTorch object that contains tokenized and encoded versions of raw data
+    """
     TOKENIZER = CamembertTokenizer.from_pretrained(
         'camembert-base', do_lower_case=True)
     encoded_batch = TOKENIZER.batch_encode_plus(raw_texts,
@@ -37,6 +51,15 @@ def preprocess(raw_texts, labels=None):
 
 
 def predict(texts, model):
+    """ Do predictions.
+
+    Args:
+        texts (array-like): a list of texts
+        model (CamembertForSequenceClassification): trained model
+
+    Returns:
+        predictions
+    """
     with torch.no_grad():
         model.eval()
         input_ids, attention_mask = preprocess(texts)
